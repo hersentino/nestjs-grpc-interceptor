6c1257ce0a0735f2688067c9915882b5118a16b3
Francois HERSENT <francois.hersent@hiventive.com>
2024-07-23 14:37:52 +0200

    first commit

--- src/interceptor.ts
+++ src/interceptor.ts
@@ -4,7 +4,6 @@ import {
   Injectable,
   NestInterceptor,
 } from '@nestjs/common';
-import { catchError, tap, throwError } from 'rxjs';
 
 @Injectable()
 export default class LoggerInterceptor implements NestInterceptor {
@@ -17,19 +16,6 @@ export default class LoggerInterceptor implements NestInterceptor {
     console.info(
       `REQ: [${contextType}]:[${controllerName}]:[${method}]:-> ${JSON.stringify(reqBody)}`,
     );
-    return next.handle().pipe(
-      tap((data) => {
-        console.info(
-          `RES: [${contextType}]:[${controllerName}]:[${method}]:-> ${JSON.stringify(data)}`,
-        );
-      }),
-      catchError((error) => {
-        console.error(
-          `ERR: [${contextType}]:[${controllerName}]:[${method}]:-> ${error.message}`,
-          error.stack,
-        );
-        return throwError(() => error);
-      }),
-    );
+    return next.handle();
   }
 }
